91b1024a4dfc2ee184a7f5150c4432b6b0f1a474
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-10-20 03:38:32 -0700

    socialButtons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {FormGroup, InputGroup, FormControl, Button} from 'react-bootstrap';
+import { FormGroup, InputGroup, FormControl, Button } from 'react-bootstrap';
 
 const Header = () => {
   return (
@@ -13,33 +13,32 @@ const Header = () => {
     </div>
   );
 };
-const Form = () =>
-{
-  return(
+const Form = () => {
+  return (
     <form className="form-horizontal" method="post" action="#">
-      <Header />
+
       <FormGroup>
         <InputGroup>
           <InputGroup.Addon><i className="fa fa-user-o" aria-hidden="true"></i></InputGroup.Addon>
-          <FormControl type="text" placeholder="Username"/>
+          <FormControl type="text" placeholder="Username" />
         </InputGroup>
       </FormGroup>
       <FormGroup>
         <InputGroup>
           <InputGroup.Addon>@</InputGroup.Addon>
-          <FormControl type="text" placeholder="Email"/>
+          <FormControl type="text" placeholder="Email" />
         </InputGroup>
       </FormGroup>
       <FormGroup>
         <InputGroup>
           <InputGroup.Addon><i className="fa fa-lock" aria-hidden="true"></i></InputGroup.Addon>
-          <FormControl type="text" placeholder="Password"/>
+          <FormControl type="text" placeholder="Password" />
         </InputGroup>
       </FormGroup>
       <FormGroup>
         <InputGroup>
           <InputGroup.Addon><i className="fa fa-lock" aria-hidden="true"></i></InputGroup.Addon>
-          <FormControl type="text" placeholder="Repeat password"/>
+          <FormControl type="text" placeholder="Repeat password" />
         </InputGroup>
       </FormGroup>
       <FormGroup>
@@ -48,17 +47,32 @@ const Form = () =>
     </form>
   );
 };
-
+const ButtonSocial = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-6 text-center">
+          <Button className="socialBtn" bsStyle="primary" block><i class="fa fa-facebook-official" aria-hidden="true"></i>  Facebook</Button>
+        </div>
+        <div className="col-md-6">
+          <Button className="socialBtn" bsStyle="danger" block><i class="fa fa-google-plus-square" aria-hidden="true"></i>  Google</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
 class App extends Component {
   render() {
     return (
-        <div className="container">
-          <div className="row main">
-            <div className="main-login main-center">
-              <Form />
-            </div>
+      <div className="container">
+        <div className="row main">
+          <div className="main-login main-center">
+            <Header />
+            <Form />
+            <ButtonSocial />
           </div>
         </div>
+      </div>
 
     );
   }
